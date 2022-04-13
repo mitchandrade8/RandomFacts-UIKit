61fa582655619230098f0f4b9aba5c3ae0c86398
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-13 15:50:53 -0600

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Mitch Andrade on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
